e2e5be6a85f3f2e21dc1463190bfbf4c184d09d5
Jeton Rama <Jeton.Rama97@gmail.com>
2026-04-15 21:10:33 +0200

    feat: Lasttests mit locust hinzugefügt

---
+++ tests/lasttest/locustfile.py
@@ -0,0 +1,35 @@
+"""Lasttest mit Locust."""
+
+from typing import Final, Literal
+
+import urllib3
+from locust import HttpUser, constant_throughput, task
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class GetUser(HttpUser):
+    """Lasttest für GET-Requests."""
+
+    wait_time = constant_throughput(0.1)
+
+    def on_start(self) -> None:
+        """Token holen."""
+        self.client.verify = False
+        response: Final = self.client.post(
+            url="/auth/token", json={"username": "admin", "password": "p"}
+        )
+        body: Final[dict[Literal["token"], str]] = response.json()
+        token: Final = body["token"]
+        self.client.headers = {"Authorization": f"Bearer {token}"}
+
+    @task(100)
+    def get_by_id(self) -> None:
+        """GET-Requests mit ID."""
+        for mitglied_id in [1, 20, 30, 40, 50, 60]:
+            self.client.get(f"/rest/{mitglied_id}")
+
+    @task(200)
+    def get_alle(self) -> None:
+        """GET-Requests alle Mitglieder."""
+        self.client.get("/rest")
